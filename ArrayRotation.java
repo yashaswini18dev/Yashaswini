cba72496c60a53ea27c7e39bd676d8beee2bc6cd
yashaswini18dev <139639021+yashaswini18dev@users.noreply.github.com>
2023-07-15 23:41:02 +0530

    Add files via upload

---
+++ ArrayRotation.java
@@ -0,0 +1,34 @@
+import java.util.*;
+ 
+class ArrayRotation
+{
+ 
+static void RightRotate(int a[],int n, int k)
+                        
+{
+     
+    k=k%n;
+ 
+    for(int i = 0; i < n; i++)
+    {
+        if(i<k) {
+                  System.out.print(a[n + i - k]+ " ");
+                   }
+        else  {
+       
+            System.out.print(a[i - k]+ " ");
+                   }
+    }
+    System.out.println();
+}
+     
+public static void main(String args[])
+{
+    int Array[] = {1, 2, 3, 4, 5};
+    int N = Array.length;
+ 
+    int K = 2;
+    RightRotate(Array, N, K);
+ 
+}
+}
